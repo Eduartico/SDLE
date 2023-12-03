bcf3d799c1a0c5420ef07d2185c6c6650a5cf5af
Alexandre Correia <correiaalexandrealex@gmail.com>
2023-12-03 19:09:56 +0000

    updated API service in frontend

--- my-shopping-list-app/src/services/ApiService.js
+++ my-shopping-list-app/src/services/ApiService.js
@@ -21,7 +21,49 @@ const ApiService = {
   getListById: async (listId) => {
     const response = await axios.get(`${BASE_URL}/list/${listId}`);
     return response.data;
-  }
+  },
+
+  updateListById: async (request) => {
+    // Request example:
+    // {"name": "My List", "listId": "1", "isRecipe": false}
+    const response = await axios.put(`${BASE_URL}/updateList`, request);
+    return response.data;
+  },
+
+  updateListItemById: async (request) => {
+    // Request example:
+    // {"name": "My Item", "itemId": "1", "quantity": 3, "boughtQuantity": 2}
+    const response = await axios.put(`${BASE_URL}/updateListItem`, request);
+    return response.data;
+  },
+
+  createList: async (list) => {
+    // Request example:
+    // {"name": "My List", "userId": "1", "isRecipe": false}
+    const response = await axios.post(`${BASE_URL}/addList`, list);
+    return response.data;
+  },
+
+  createListItem: async (request) => {
+    // Request example:
+    // {"name": "My Item", "listId": "1", "quantity": 3, "boughtQuantity": 2}
+    const response = await axios.post(`${BASE_URL}/addListItem`, request);
+    return response.data;
+  },
+  
+  deleteListById: async (request) => {
+    // Request example:
+    // {"listId": "1"}
+    const response = await axios.delete(`${BASE_URL}/deleteList`, request);
+    return response.data;
+  },
+
+  deleteListItemById: async (request) => {
+    // Request example:
+    // {"itemId": "1"}
+    const response = await axios.delete(`${BASE_URL}/deleteListItem`, request);
+    return response.data;
+  },
 
 };
 
