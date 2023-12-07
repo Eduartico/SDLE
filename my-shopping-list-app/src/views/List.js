d3cbc49f497b8d5a1360f80d52e9334cb748a04f
up202004999 <duarte.eduardoeds@gmail.com>
2023-12-07 18:52:27 +0000

    Remade lists view

--- my-shopping-list-app/src/views/List.js
+++ my-shopping-list-app/src/views/List.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ApiService from '../services/ApiService';
-import BottomAppBar from "../components/BottomAppBar";
-
+import BottomAppBar from '../components/BottomAppBar';
 
 const List = () => {
   const { listId } = useParams();
@@ -30,7 +29,22 @@ const List = () => {
     setList((prevList) => {
       const updatedItems = prevList.items.map((item) => {
         if (item.id === itemId) {
-          return { ...item, boughtQuantity: newQuantity };
+          const boughtQuantity = Math.max(0, Math.min(newQuantity, item.quantity));
+          return { ...item, boughtQuantity };
+        }
+        return item;
+      });
+
+      return { ...prevList, items: updatedItems };
+    });
+  };
+
+  const handleToggleDone = (itemId) => {
+    setList((prevList) => {
+      const updatedItems = prevList.items.map((item) => {
+        if (item.id === itemId) {
+          const newBoughtQuantity = item.boughtQuantity === item.quantity ? 0 : item.quantity;
+          return { ...item, boughtQuantity: newBoughtQuantity };
         }
         return item;
       });
@@ -51,47 +65,112 @@ const List = () => {
     <div>
       <h2>List Name: {list.name}</h2>
 
+      <h3>Done</h3>
       <ul>
-        {list.items.map((item) => (
-          <li key={item.id}>
-            <div>
-              <strong>{item.name}</strong>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={item.boughtQuantity === item.quantity}
-                  onChange={() => handleQuantityChange(item.id, item.quantity)}
-                />
-                Bought
-              </label>
-
-              {item.quantity > 1 && (
+        {list.items
+          .filter((item) => item.boughtQuantity === item.quantity)
+          .map((item) => (
+            <li key={item.id}>
+              <div style={{ display: 'flex', alignItems: 'inline' }}>
+                <strong>{item.name}</strong>
+                <div style={{ cursor: 'pointer', marginLeft: '5px' }} > Needed:  {item.quantity} </div>
                 <label>
-                  Quantity:
-                  <select
-                    value={item.boughtQuantity}
-                    onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value, 10))}
-                  >
-                    {[...Array(item.quantity).keys()].map((num) => (
-                      <option key={num + 1} value={num + 1}>
-                        {num + 1}
-                      </option>
-                    ))}
-                  </select>
+                  <input
+                    type="checkbox"
+                    checked={item.boughtQuantity === item.quantity}
+                    onChange={() => handleToggleDone(item.id)}
+                  />
+                  Bought:
                 </label>
-              )}
-            </div>
-          </li>
-        ))}
+                {item.quantity > 1 && (
+                  <label>
+                    <div>
+                      <button
+                        onClick={() => handleQuantityChange(item.id, Math.max(1, item.boughtQuantity - 1))}
+                        style={{ cursor: 'pointer', marginLeft: '5px' }}
+                      >
+                        -
+                      </button>
+                      <input
+                        type="number"
+                        value={item.boughtQuantity}
+                        onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value, 10))}
+                        min="0"
+                        max={item.quantity}
+                        inputMode="numeric"
+                        style={{ width: '50px', textAlign: 'center' }}
+                      />
+                      <button
+                        onClick={() => handleQuantityChange(item.id, item.boughtQuantity + 1)}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </label>
+                )}
+              </div>
+            </li>
+          ))}
+      </ul>
+      <h3>Needed</h3>
+      <ul>
+        {list.items
+          .filter((item) => item.boughtQuantity < item.quantity)
+          .map((item) => (
+            <li key={item.id}>
+              <div style={{ display: 'flex', alignItems: 'inline' }}>
+                <strong>{item.name}</strong>
+                <div style={{ cursor: 'pointer', marginLeft: '5px' }} > Needed:  {item.quantity} </div>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={item.boughtQuantity === item.quantity}
+                    onChange={() => handleToggleDone(item.id)}
+                  />
+                  Bought:
+                </label>
+                {item.quantity > 1 && (
+                  <label>
+                    <div>
+                      <button
+                        onClick={() =>
+                          handleQuantityChange(item.id, Math.max(1, item.boughtQuantity - 1))
+                        }
+                        style={{ cursor: 'pointer', marginLeft: '5px' }}
+                      >
+                        -
+                      </button>
+                      <input
+                        type="number"
+                        value={item.boughtQuantity}
+                        onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value, 10))}
+                        min="0"
+                        max={item.quantity}
+                        inputMode="numeric"
+                        style={{ width: '50px', textAlign: 'center' }}
+                      />
+                      <button
+                        onClick={() => handleQuantityChange(item.id, item.boughtQuantity + 1)}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </label>
+                )}
+              </div>
+            </li>
+          ))}
       </ul>
       <div
         style={{
-          position: "fixed",
+          position: 'fixed',
           bottom: 0,
           left: 0,
-          width: "100%",
-          background: "#f0f0f0",
-          padding: "10px",
+          width: '100%',
+          background: '#f0f0f0',
+          padding: '10px',
         }}
       >
         <BottomAppBar />
