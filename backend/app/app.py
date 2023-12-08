9f08789878298cf075098175dc7987484eb752f9
Alexandre Correia <correiaalexandrealex@gmail.com>
2023-12-08 16:40:33 +0000

    fix profile name not showing

--- backend/app/app.py
+++ backend/app/app.py
@@ -136,7 +136,7 @@ def get_current_user():
             abort(404)
         data = {
             'user': {
-                'username': username,
+                'name': username,
                 'id': user_id
             }
         }
